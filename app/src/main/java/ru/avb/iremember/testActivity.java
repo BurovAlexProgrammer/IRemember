6feec0e5a76fade69a09de61af2c42ef3bf6d879
Aleksey Burov <burovalexprogrammer@gmail.com>
2016-12-08 13:29:41 +0500

    Include mat color palette 80%

--- app/src/main/java/ru/avb/iremember/testActivity.java
+++ app/src/main/java/ru/avb/iremember/testActivity.java
@@ -9,16 +9,22 @@ import android.graphics.ColorFilter;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.util.DisplayMetrics;
 import android.view.Display;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import java.lang.reflect.Array;
 import java.util.Random;
+import java.util.zip.Inflater;
 
 /**
  * Created by Alex on 06.12.2016.
@@ -43,14 +49,15 @@ public class testActivity extends Activity {
         grid = (GridView)findViewById(R.id.grid);
         grid.setAdapter(new ImageAdapter(this));
         adjustGridView();
+        //G.Log("Color count:"+);
     }
 
     private void adjustGridView() {
-        grid.setNumColumns(GridView.AUTO_FIT);
+        //grid.setNumColumns(GridView.AUTO_FIT);
         //grid.setColumnWidth(50);
-        grid.setVerticalSpacing(8);
-        grid.setHorizontalSpacing(8);
-        grid.setNumColumns(5);
+        //grid.setVerticalSpacing(8);
+        //grid.setHorizontalSpacing(8);
+        grid.setNumColumns(7);
         grid.setStretchMode(GridView.STRETCH_COLUMN_WIDTH);
     }
 
@@ -65,7 +72,8 @@ public class testActivity extends Activity {
         }
 
         public int getCount() {
-            return categoryIconIDs.length;
+            return matColorsInt.length ;
+            //return categoryIconIDs.length;
         }
 
         public Object getItem(int position) {
@@ -73,37 +81,40 @@ public class testActivity extends Activity {
         }
 
         public long getItemId(int position) {
-            return categoryIconIDs[position];
+            return 0;
+            //return matColors.getColor(position, Color.BLACK);
+            //return categoryIconIDs[position];
         }
 
         // create a new ImageView for each item referenced by the Adapter
         public View getView(int position, View convertView, ViewGroup parent) {
+            //LinearLayout cardView = (LinearLayout)findViewById(R.id.root);
+
+            //G.Log(cardView.toString());
             ImageView imageView;
             if (parent!=null) parent.setPadding(space,0,space,space);
             if (convertView == null) {
-                // if it's not recycled, initialize some attributes
-                imageView = new ImageView(mContext);
-                imageView.setLayoutParams(new GridView.LayoutParams(85,85));
-                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-                //imageView.setPadding(16,16,16,16);
-                imageView.setPadding(8, 8, 8, 8);
-                //imageView.setImageTintMode(PorterDuff.Mode.MULTIPLY);
-
-                //=======================TINT MODE=========================
-                //Random random=new Random();
-                //ColorFilter cf = new PorterDuffColorFilter(Color.rgb(random.nextInt(255), random.nextInt(255), random.nextInt(255)), PorterDuff.Mode.MULTIPLY);
-                //imageView.setImageResource(R.drawable.cat_bus);
-                //imageView.setColorFilter(cf);
-                //========================MAT COLOR PALETTE====================================
-
-
-
-            } else {
-                imageView = (ImageView) convertView;
+                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.item_icon_gallery, parent, false);
+                ImageView img = (ImageView)convertView.findViewById(R.id.image);
             }
 
-            imageView.setImageResource(categoryIconIDs[position]);
-            return imageView;
+            ImageView img = (ImageView)convertView.findViewById(R.id.image);
+            TextView text = (TextView)convertView.findViewById(R.id.text);
+
+            text.setText("");
+            //img.setLayoutParams();
+            img.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            //img.setBackgroundColor(matColorsInt[position]);
+
+            Random random=new Random();
+            int currColor = Color.parseColor(matColors.getString(position));
+            ColorFilter cf = new PorterDuffColorFilter(currColor, PorterDuff.Mode.MULTIPLY);
+            img.setImageResource(R.drawable.cat_bus);
+            img.setColorFilter(cf);
+
+            //imageView.setImageResource(categoryIconIDs[position]);
+            return convertView;
         }
 
         // references to our images
@@ -117,29 +128,23 @@ public class testActivity extends Activity {
                 R.drawable.cat_truck
         };
 
-        private TypedArray matColors = getResources().obtainTypedArray(R.array.mat_colors);
-
+        private TypedArray matColors = getResources().obtainTypedArray(R.array.mat_color_array);
+        int[] matColorsInt = getResources().getIntArray(R.array.mat_color_array);
     }
 
     void printSecreenInfo(){
-
         Display display = getWindowManager().getDefaultDisplay();
         DisplayMetrics metrics = new DisplayMetrics();
         display.getMetrics(metrics);
         G.Log("density :" +  metrics.density);
-
         // density interms of dpi
         G.Log("D density :" +  metrics.densityDpi);
-
         // horizontal pixel resolution
         G.Log("width pix :" +  metrics.widthPixels);
-
         // actual horizontal dpi
         G.Log("xdpi :" +  metrics.xdpi);
-
         // actual vertical dpi
         G.Log("ydpi :" +  metrics.ydpi);
-
     }
 
     public static String getDensityBucket(Resources resources) {
